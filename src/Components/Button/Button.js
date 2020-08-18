fed4894d5aa1542493e5a41eaa3da5b7df383023
Emelie Edman <emelie@MacBook-Pro.local>
2020-08-18 09:44:26 +0200

    added turnery operator for the type of props the button is receiving, img or text

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -3,7 +3,9 @@ import styles from './Button.module.css'
 
 const Button = (props) => {
     return(
+    props.img ? 
     <button type='button' id="button" className={styles.button}><img src={props.img} alt="" width="30px"/></button>
+    : <button type='button' id="button" className={styles.button}>{props.text}</button>
     )
 }
 
