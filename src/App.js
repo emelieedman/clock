22fa6f68b1b388f18583f01dcba54f1882bfda15
Emelie Edman <emelie@MacBook-Pro.local>
2020-08-18 14:14:15 +0200

    added turnery operators for counting sec, min & hours + reset function onclick

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import stopwatchImg from "./Assets/stopwatch.svg";
 import stopwatchImgActive from "./Assets/stopwatch-active.svg";
 import timerImg from "./Assets/timer.svg";
 import timerImgActive from "./Assets/timer-active.svg";
-import { useTimer, elapsedTime } from "./customHooks";
 
 function App() {
   const [isRunning, setIsRunning] = useState(false);
-  const [elapsedTime, setElapsedTime] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
 
   function toggle() {
     setIsRunning(!isRunning);
@@ -22,7 +23,7 @@ function App() {
   }
 
   function reset() {
-    setElapsedTime(0);
+    setSeconds(0);
     setIsRunning(false);
   }
 
@@ -30,25 +31,25 @@ function App() {
     let interval = null;
     if (isRunning) {
       interval = setInterval(() => {
-        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+        setSeconds((prevseconds) => prevseconds + 1);
       }, 1000);
-    } else if (!isRunning && elapsedTime !== 0) {
+    } else if (!isRunning && seconds !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isRunning, elapsedTime]);
+  }, [isRunning, seconds]);
 
   return (
     <div className="App">
       <div className="digital-boxes">
-        <DigitalBox nr="00" />
-        <DigitalBox nr="00" />
-        <DigitalBox nr={elapsedTime} />
+        <DigitalBox nr={minutes > 60 ? setHours(hours + 1) : hours} />
+        <DigitalBox nr={seconds > 60 ? setMinutes(minutes + 1) : minutes} />
+        <DigitalBox nr={seconds <= 60 ? seconds : setSeconds(0)} />
       </div>
 
       <div className="ellipse"></div>
       <div className="start-reset-buttons">
-        <Button text="Reset" />
+        <Button text="Reset" onClick={reset} />
         <Button text={isRunning ? "Stop" : "Start"} onClick={toggle} />
       </div>
       <div className="navigation-buttons">
