7220cd4ada688decc67aa8542d3cb49d3cf67bcb
Emelie Edman <emelie@MacBook-Pro.local>
2020-08-18 14:59:49 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -10,48 +10,15 @@ import stopwatchImg from "./Assets/stopwatch.svg";
 import stopwatchImgActive from "./Assets/stopwatch-active.svg";
 import timerImg from "./Assets/timer.svg";
 import timerImgActive from "./Assets/timer-active.svg";
+import Stopwatch from "./Components/Pages/Stopwatch/Stopwatch";
 
 function App() {
-  const [isRunning, setIsRunning] = useState(false);
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
-
-  function toggle() {
-    setIsRunning(!isRunning);
-    console.log("is running?", !isRunning);
-  }
-
-  function reset() {
-    setSeconds(0);
-    setIsRunning(false);
-  }
-
-  useEffect(() => {
-    let interval = null;
-    if (isRunning) {
-      interval = setInterval(() => {
-        setSeconds((prevseconds) => prevseconds + 1);
-      }, 1000);
-    } else if (!isRunning && seconds !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isRunning, seconds]);
-
   return (
     <div className="App">
-      <div className="digital-boxes">
-        <DigitalBox nr={minutes > 60 ? setHours(hours + 1) : hours} />
-        <DigitalBox nr={seconds > 60 ? setMinutes(minutes + 1) : minutes} />
-        <DigitalBox nr={seconds <= 60 ? seconds : setSeconds(0)} />
-      </div>
+      <Stopwatch />
 
       <div className="ellipse"></div>
-      <div className="start-reset-buttons">
-        <Button text="Reset" onClick={reset} />
-        <Button text={isRunning ? "Stop" : "Start"} onClick={toggle} />
-      </div>
+
       <div className="navigation-buttons">
         <Button img={clockImg} />
         <Button img={alarmImg} />
---
+++ src/Components/Pages/Stopwatch/Stopwatch.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Stopwatch.module.css";
+import DigitalBox from "../../DigitalBox/DigitalBox";
+import Button from "../../Button/Button";
+
+const Stopwatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  function toggle() {
+    setIsRunning(!isRunning);
+    console.log("is running?", !isRunning);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsRunning(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setSeconds(seconds + 1);
+      }, 1000);
+    } else if (!isRunning && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning, seconds]);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.digitalBoxes}>
+        <DigitalBox nr={minutes > 60 ? setHours(hours + 1) : hours} />
+        <DigitalBox nr={seconds > 60 ? setMinutes(minutes + 1) : minutes} />
+        <DigitalBox nr={seconds <= 60 ? seconds : setSeconds(0)} />
+      </div>
+      <div className={styles.startResetButtons}>
+        <Button text="Reset" onClick={reset} />
+        <Button text={isRunning ? "Stop" : "Start"} onClick={toggle} />
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
