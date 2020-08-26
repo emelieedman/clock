32562326b77f63eceb3e6532c7889d37335b6d27
Emelie Edman <emelie@MacBook-Pro.local>
2020-08-26 21:57:58 +0200

    changed size

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Button = ({ onClick, img, text, id }) => {
   return (
     <Link to={id} type="button" className={styles.button} onClick={onClick}>
-      {img ? <img src={img} alt="" width="30px" /> : text}
+      {img ? <img src={img} alt="" width="25px" /> : text}
     </Link>
   );
 };
