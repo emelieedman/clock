27656ddb77ff9cce260dbd86b3448c2a64292d5e
Emelie Edman <emelie@MacBook-Pro.local>
2020-09-01 15:51:01 +0200

    fixed styling button

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -3,15 +3,18 @@ import React from "react";
 import styles from "./Button.module.css";
 import { Link } from "react-router-dom";
 
-const Button = ({ onClick, img, text, id }) => {
+const Button = ({ onClick, text }) => {
   return (
-    <Link to={id} type="button" className={styles.button} onClick={onClick}>
-      {img ? (
-        <img src={img} alt="" width="25px" className={styles.img} />
-      ) : (
-        text
-      )}
-    </Link>
+    // <Link to={id} type="button" className={styles.button} onClick={onClick}>
+    //   {img ? (
+    //     <img src={img} alt="" width="25px" className={styles.img} />
+    //   ) : (
+    //     text
+    //   )}
+    // </Link>
+    <button className={styles.button} onClick={onClick}>
+      {text}
+    </button>
   );
 };
 
